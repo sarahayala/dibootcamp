c780a539ae1b1a1832a8a1269b6b4042d306b28c
sarah <sarah@togoshoes.co.il>
2023-12-19 21:10:04 +0200

    week3 day1

---
+++ week3/day1/script.js
@@ -0,0 +1,36 @@
+let thing = 'hello world'
+
+
+console.log(thing);
+
+
+let a = 5
+let b;
+let c = 5+20
+let d = null;
+let e = true;
+
+
+// console.log(a+b);
+console.log(a+thing);
+console.log(a+c);
+
+console.log(a*c+(b+a-1));
+
+console.log(null);
+console.log(true);
+
+let longString = "This is a very long string which needs " +
+                 "to wrap across multiple lines because " +
+                 "otherwise, my code is unreadable.";
+
+console.log (longString.length);
+
+let string = `look it's the number ${a*5}`;
+console.log (string.indexOf ('look'));
+
+let f = 62 
+let g =' hertzel st';
+let h = 'israel' ;
+
+console.log(f+g+h);
---
+++ week3/day1/script2.js
@@ -0,0 +1,27 @@
+let name= 'value'
+let str= 'text'
+let num= 6
+let bool= true
+let un;
+let v= null 
+let aray= [12, 13 ,14, 15]
+let object ={ key: 'value',
+    name: 'nadav'
+}
+   
+
+console.log(str+num)
+console. log('hello class')
+
+let str2 = 'hello'
+let str3= "hello it's"
+let str4= 'hello'
+let str5=  `${str} text`
+
+console.log(str4)
+let users = (sara)
+
+
+console. log (users [0]);
+users.push('sarah')
+console. log (users [0]);
