878090043b7a716e891c1ab21b703dcfd05a9e6c
sarah <sarah@togoshoes.co.il>
2023-12-28 21:19:00 +0200

    week3 day5

---
+++ test/script1.js
@@ -0,0 +1,21 @@
+//alert ('hello this is in alert')
+
+// let conf = confirm('are you lerning?')
+// if (conf==true){
+//     alert('good job!')
+
+// } else {
+//     alert('no gooooood!')
+// }
+let prop = prompt('what is yuor favorite number?')
+if (!(isNaN(prop))){
+
+}
+if ( prop == 7) {
+    alert('hey sarah!')
+
+} else if (prop == 5){
+    alert('hey nadav')
+} else{
+    alert('hey guest');
+}
